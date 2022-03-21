b2a9a4a7e12ce761d5ef167c1cd00e2c814f03dc
lgvv <lgw9898@naver.com>
2022-03-21 12:48:14 +0900

    [수정] Global 파일에서 사용하지 않는 변수를 제거하였습니다. (최종적으로는 Global 파일을 삭제합니다.)

--- Kuring/Utils/Global.swift
+++ Kuring/Utils/Global.swift
@@ -13,8 +13,8 @@ let DeviceHeightRatio = UIScreen.main.bounds.size.height / 608 /// 스냅킷에�
 var originalBaseUrl = "https://www.konkuk.ac.kr/do/MessageBoard/ArticleRead.do" /// 베이스 URL (재성님 sdk에서 url을 받아올 방법을 모르겠어요
 var libraryBaseUrl = "https://library.konkuk.ac.kr/#/bbs/notice/"
 
-var articleKey = "readArticleId" /// userDefault 키(읽은 아티클 아이디)
-var readArticle = UserDefaults.standard.array(forKey: articleKey) ?? [String]() /// userDefault 읽은 아티클 id저장
+//var articleKey = "readArticleId" /// userDefault 키(읽은 아티클 아이디)
+//var readArticle = UserDefaults.standard.array(forKey: articleKey) ?? [String]() /// userDefault 읽은 아티클 id저장
 
 var timeKey = "EntryTime" /// userDefault 키(마지막으로 접속한 시간) 
 var userConnectedTime = UserDefaults.standard.string(forKey: timeKey) ?? "20211201" /// userDefault 읽은 시간 값 - default값 00000000
