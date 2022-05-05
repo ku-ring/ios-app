fc9535733252116de0a1a0f36ba030facf52553a
lgvv <lgw9898@naver.com>
2022-05-05 17:10:33 +0900

    [삭제] app단의 UserDefaultManager 삭제하고, sdk로 이전하였습니다.

--- Kuring/Manger/AppStoreReviewManager.swift
+++ Kuring/Manger/AppStoreReviewManager.swift
@@ -7,14 +7,15 @@
 
 import UIKit
 import StoreKit
+import KuringSDK
 
 class AppStoreReviewManager {
     /// action count 값이 3일 때 `true` 입니다.
-    static var isReviewable: Bool { UserDefaultManager.inAppReviewCount == 3 }
+    static var isReviewable: Bool { Kuring.inAppReviewCount == 3 }
     
     static func requestReviewIfAppropriate() {
-        guard UserDefaultManager.inAppReviewCount <= 3 else { return }
-        UserDefaultManager.inAppReviewCount += 1
+        guard Kuring.inAppReviewCount <= 3 else { return }
+        Kuring.inAppReviewCount += 1
 
         guard isReviewable else { return }
         let scene = UIApplication.shared.connectedScenes.first { $0.activationState == .foregroundActive }
