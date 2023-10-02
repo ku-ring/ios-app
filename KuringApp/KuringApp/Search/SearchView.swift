a8b8925c67d62b5603ebcec43d3883ff0ae58ab5
lgvv <lgw9898@naver.com>
2023-10-03 02:37:27 +0900

    [추가] 스태프 구현 (디벨롭 브랜치 정상화 후 스태시 불러오고 컨플릭 수정)

--- KuringApp/KuringApp/Search/SearchView.swift
+++ KuringApp/KuringApp/Search/SearchView.swift
@@ -5,6 +5,7 @@
 //  Created by Jaesung Lee on 2023/09/22.
 //
 
+import Model
 import SwiftUI
 import ComposableArchitecture
 
@@ -12,16 +13,36 @@ struct SearchFeature: Reducer {
     struct State: Equatable {
         var recents: [String] = []
         var results: [String]? = nil
+        
+        var resultNotices: [Notice]? = nil
+        var resultStaffs: [Staff]? = nil
+        
         @BindingState var searchInfo: SearchInfo = SearchInfo()
         @BindingState var focus: Field? = .search
         
         struct SearchInfo: Equatable {
             var text: String = ""
             var searchType: SearchType = .notice
+
+            var noticeSearchPhase: SearchPhase = .before
+            var staffSearchPhase: SearchPhase = .before
             
             enum SearchType: String {
                 case notice
                 case staff
+            }
+            
+            enum SearchPhase {
+                /// 검색 시작 전 (API 요청 전 상태)
+                case before
+                /// 검색 중 (API 응답을 기다리는 상태)
+                case searching
+                /// 검색 완료 (결과가 있는 상태)
+                case complete
+                /// 검색 완료 (결과가 없는 상태)
+                case completeNoResult
+                /// 검색 실패
+                case failure
             }
         }
         
@@ -44,9 +65,13 @@ struct SearchFeature: Reducer {
             case .binding:
                 return .none
                 
+            case .binding(\.$searchInfo):
+                return .none
+                
             case .deleteAllRecentsButtonTapped:
                 state.recents.removeAll()
                 return .none
+                
             }
         }
     }
@@ -58,55 +83,245 @@ struct SearchView: View {
     
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            List {
-                TextField("검색어를 입력해주세요", text: viewStore.$searchInfo.text)
-                    .focused($focus, equals: .search)
-                
-                Picker("어떤 것을 검색하시나요?", selection: viewStore.$searchInfo.searchType) {
-                    Text("공지")
-                        .tag(SearchFeature.State.SearchInfo.SearchType.notice)
-                    
-                    Text("교직원")
-                        .tag(SearchFeature.State.SearchInfo.SearchType.staff)
+            VStack(spacing: 0) {
+                ZStack {
+                    HStack(alignment: .center, spacing: 12) {
+                        if viewStore.searchInfo.text.isEmpty {
+                            Image(systemName: "magnifyingglass")
+                                .frame(width: 16, height: 16)
+                                .foregroundStyle(Color(red: 0.21, green: 0.24, blue: 0.29).opacity(0.6))
+                        }
+                        
+                        TextField("검색어를 입력해주세요", text: viewStore.$searchInfo.text)
+                            .focused($focus, equals: .search)
+                        
+                        if viewStore.searchInfo.text.isEmpty {
+                            Image(systemName: "xmark")
+                                .frame(width: 16, height: 16)
+                                .foregroundStyle(Color(red: 0.21, green: 0.24, blue: 0.29).opacity(0.6))
+                        }
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 7)
+                    .background(Color(red: 0.95, green: 0.95, blue: 0.96))
+                    .cornerRadius(20)
                 }
-                .pickerStyle(.segmented)
                 
-                Section {
+                if !viewStore.recents.isEmpty {
+                    HStack {
+                        Text("최근검색어")
+                            .font(.system(size: 14, weight: .medium))
+                            .foregroundColor(Color(red: 0.21, green: 0.24, blue: 0.29).opacity(0.6))
+                        Spacer()
+                        Button {
+                            viewStore.send(.deleteAllRecentsButtonTapped)
+                        } label: {
+                            Text("전체삭제")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(red: 0.56, green: 0.56, blue: 0.56))
+                        }
+                    }
+                    .padding(.top, 20)
+                    .padding(.bottom, 12)
+                    
                     ScrollView(.horizontal) {
                         LazyHStack {
                             ForEach(viewStore.recents, id: \.self) { recent in
-                                Button(recent) {
-                                    
+                                HStack(alignment: .center, spacing: 6) {
+                                    Button {
+                                        // TODO: 검색
+                                    } label: {
+                                        Text(recent)
+                                            .font(.system(size: 14))
+                                            .foregroundStyle(Color(red: 0.24, green: 0.74, blue: 0.5))
+                                            .padding(.horizontal, 12)
+                                            .padding(.vertical, 4)
+                                            .background(Color(red: 0.24, green: 0.74, blue: 0.5).opacity(0.1))
+                                            .cornerRadius(20)
+                                    }
                                 }
+                                
                             }
                         }
                     }
-                    
-                    Button("전체 삭제") {
-                        viewStore.send(.deleteAllRecentsButtonTapped)
-                    }
-                    .tint(.pink)
-                } header: {
-                    Text("최근 검색어")
+                    .frame(height: 30)
                 }
                 
-                Section {
-                    Text(viewStore.searchInfo.text)
+                HStack {
+                    Text("검색결과")
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(Color(red: 0.21, green: 0.24, blue: 0.29).opacity(0.6))
+                        .padding(.top, 20)
+                        .padding(.bottom, 12)
                     
-                    Text(viewStore.searchInfo.searchType.rawValue)
-                } header: {
-                    Text("검색 정보")
+                    Spacer()
                 }
                 
-                Section {
-                    if viewStore.results?.isEmpty == true {
-                        Text("검색결과가 없어요")
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(height: 50)
+                    .padding(.horizontal, 20)
+                    .background(Color(red: 0.95, green: 0.95, blue: 0.96))
+                    .cornerRadius(10)
+                    .overlay {
+                        switch viewStore.searchInfo.searchType {
+                        case .notice:
+                            HStack {
+                                segmentView("공지", isSelect: true)
+                                
+                                Button {
+                                    let type = SearchFeature.State.SearchInfo(searchType: .staff)
+                                    viewStore.send(.binding(.set(\.$searchInfo, type)))
+                                } label: {
+                                    segmentView("교직원", isSelect: false)
+                                }
+                            }
+                            .padding(5)
+                            
+                        case .staff:
+                            HStack {
+                                Button {
+                                    let type = SearchFeature.State.SearchInfo(searchType: .notice)
+                                    viewStore.send(.binding(.set(\.$searchInfo, type)))
+                                } label: {
+                                    segmentView("공지", isSelect: false)
+                                }
+                                
+                                segmentView("교직원", isSelect: true)
+                            }
+                            .padding(5)
+                        }
+                    }
+                
+                switch viewStore.searchInfo.searchType {
+                case .notice:
+                    if let notices = viewStore.resultNotices {
+                        if notices.isEmpty {
+                            feedbackPhaseView
+                        } else {
+                            List(notices, id: \.self) { notice in
+                                // TODO: - 디자인 시스템 NoticeRow
+                                Text("NoticeRow 적용하기")
+                            }
+                        }
+                    } else {
+                        emptyPhaseView(viewStore.searchInfo.noticeSearchPhase)
+                    }
+                case .staff:
+                    if let staffs = viewStore.resultStaffs {
+                        if staffs.isEmpty {
+                            feedbackPhaseView
+                        } else {
+                            List(staffs, id: \.self) { staff in
+                                Button {
+                                    // TODO: -
+                                } label: {
+                                    StaffRow(staff: staff)
+                                }
+                            }
+                        }
+                    } else {
+                        emptyPhaseView(viewStore.searchInfo.noticeSearchPhase)
                     }
                 }
+                
             }
+            .padding(.horizontal, 20)
             .bind(viewStore.$focus, to: self.$focus)
         }
     }
+    
+    @ViewBuilder
+    private func segmentView(_ title: String, isSelect: Bool) -> some View {
+        if isSelect {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.white)
+                .shadow(color: .black.opacity(0.1), radius: 6, x: 0, y: 0)
+                .overlay {
+                    Text(title)
+                        .font(.system(size: 16, weight: .bold))
+                        .foregroundStyle(Color(red: 0.24, green: 0.74, blue: 0.5))
+                }
+        } else {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.clear)
+                .overlay {
+                    Text(title)
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundStyle(Color(red: 0.21, green: 0.24, blue: 0.29).opacity(0.6))
+                }
+        }
+    }
+    
+    @ViewBuilder
+    private func emptyPhaseView(_ phase: SearchFeature.State.SearchInfo.SearchPhase) -> some View {
+        switch phase {
+        case .before:
+            beforePhaseView
+        case .searching:
+            searchingPhaseView
+        case .complete, .completeNoResult, .failure:
+            feedbackPhaseView
+        }
+    }
+    
+    @ViewBuilder
+    private var beforePhaseView: some View {
+        VStack {
+            Group {
+                Text("쿠링에서는 원하는 공지와 교직원을 한눈에")
+                Text("최고의 알고리즘으로 원하는 결과를 빠르게 찾아드려요.")
+            }
+            .foregroundColor(Color(red: 0.56, green: 0.56, blue: 0.56))
+            .font(.system(size: 16))
+                
+            Spacer()
+        }
+        .padding(.top, 72)
+    }
+    
+    @ViewBuilder
+    private var feedbackPhaseView: some View {
+        VStack {
+            Group {
+                Text("찾은 결과가 없어요.")
+                Text("검색에 문제가 있는 것 같다면 피드백을 보내주세요.")
+                Text("보내주신 피드백은 글자 한 톨까지 꼼꼼하게 쿠링팀이 살펴볼게요.")
+            }
+            .foregroundColor(Color(red: 0.56, green: 0.56, blue: 0.56))
+            .font(.system(size: 12))
+            
+            Button {
+                // TODO: 피드백
+            } label: {
+                RoundedRectangle(cornerRadius: 100)
+                    .foregroundStyle(Color(red: 0.24, green: 0.74, blue: 0.5).opacity(0.15))
+                    .frame(height: 50)
+                    .overlay {
+                        Text("피드백 보내기")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundStyle(Color(red: 0.24, green: 0.74, blue: 0.5))
+                    }
+            }
+            .padding(.top, 36)
+            
+            Spacer()
+        }
+        .padding(.top, 72)
+    }
+    
+    @ViewBuilder
+    private var searchingPhaseView: some View {
+        VStack {
+            Text("검색중...")
+                .foregroundColor(Color(red: 0.56, green: 0.56, blue: 0.56))
+                .font(.system(size: 16))
+            
+            Spacer()
+        }
+        .padding(.top, 72)
+    }
+    
 }
 
 #Preview {
@@ -117,6 +332,54 @@ struct SearchView: View {
                 reducer: { SearchFeature() }
             )
         )
+        .navigationBarTitleDisplayMode(.inline)
         .navigationTitle("Search View")
     }
 }
+
+//List {
+//    TextField("검색어를 입력해주세요", text: viewStore.$searchInfo.text)
+//        .focused($focus, equals: .search)
+//    
+//    Picker("어떤 것을 검색하시나요?", selection: viewStore.$searchInfo.searchType) {
+//        Text("공지")
+//            .tag(SearchFeature.State.SearchInfo.SearchType.notice)
+//        
+//        Text("교직원")
+//            .tag(SearchFeature.State.SearchInfo.SearchType.staff)
+//    }
+//    .pickerStyle(.segmented)
+//    
+//    Section {
+//        ScrollView(.horizontal) {
+//            LazyHStack {
+//                ForEach(viewStore.recents, id: \.self) { recent in
+//                    Button(recent) {
+//                        
+//                    }
+//                }
+//            }
+//        }
+//        
+//        Button("전체 삭제") {
+//            viewStore.send(.deleteAllRecentsButtonTapped)
+//        }
+//        .tint(.pink)
+//    } header: {
+//        Text("최근 검색어")
+//    }
+//    
+//    Section {
+//        Text(viewStore.searchInfo.text)
+//        
+//        Text(viewStore.searchInfo.searchType.rawValue)
+//    } header: {
+//        Text("검색 정보")
+//    }
+//    
+//    Section {
+//        if viewStore.results?.isEmpty == true {
+//            Text("검색결과가 없어요")
+//        }
+//    }
+//}
---
+++ KuringApp/KuringApp/Search/StaffRow.swift
@@ -0,0 +1,29 @@
+//
+//  StaffRow.swift
+//  KuringApp
+//
+//  Created by 🏝️ GeonWoo Lee on 10/3/23.
+//
+import Model
+import SwiftUI
+
+struct StaffRow: View {
+    let staff: Staff
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(staff.name)
+                .font(.subheadline)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .contentShape(Rectangle())
+            
+            Text("\(staff.deptName) · \(staff.collegeName)")
+                .font(.caption)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .contentShape(Rectangle())
+        }
+        .padding(.vertical, 8)
+        .padding(.horizontal, 20)
+    }
+}
+
