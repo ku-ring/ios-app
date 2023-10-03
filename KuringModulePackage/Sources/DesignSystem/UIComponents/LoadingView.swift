b03624ea4a5e87f2e665b766b9a6c75ca9dc5b17
lgvv <lgw9898@naver.com>
2023-10-04 00:41:08 +0900

    [추가] 로딩뷰 구현 (로티 대체)

--- KuringModulePackage/Sources/DesignSystem/UIComponents/LoadingView.swift
+++ KuringModulePackage/Sources/DesignSystem/UIComponents/LoadingView.swift
@@ -11,11 +11,35 @@ import ColorSet
 public struct LoadingView: View {
     let size: CGSize = CGSize(width: 10, height: 10)
     
+    @State var isAnimating1: Bool = false
+    @State var isAnimating2: Bool = false
+    @State var isAnimating3: Bool = false
+    
     public var body: some View {
         HStack(spacing: 5) {
             circle
+                .offset(y: isAnimating1 ? 12 : -5)
+                .animation(.easeInOut(duration: 0.5).repeatForever(),
+                           value: isAnimating1)
             circle
+                .offset(y: isAnimating2 ? 12 : -5)
+                .animation(.easeInOut(duration: 0.5).repeatForever(),
+                           value: isAnimating2)
             circle
+                .offset(y: isAnimating3 ? 12 : -5)
+                .animation(.easeInOut(duration: 0.5).repeatForever(),
+                           value: isAnimating3)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                isAnimating1 = true
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                isAnimating2 = true
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                isAnimating3 = true
+            }
         }
     }
     
