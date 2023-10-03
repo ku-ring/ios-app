a07cb19d5d3c5c4d583b0db919fc5e867429b6b6
lgvv <lgw9898@naver.com>
2023-10-03 23:34:26 +0900

    [수정] 쿠링 디자인 시스템 TextField 구현

--- KuringModulePackage/Sources/DesignSystem/UIComponents/SearchField.swift
+++ KuringModulePackage/Sources/DesignSystem/UIComponents/SearchField.swift
@@ -6,65 +6,95 @@
 //
 
 import SwiftUI
-import ColorSet
+import Combine
 
-public struct SearchFiled: View {
-    let placeHolder: String
-    @Binding var searchText: String
-    @Binding var clearButtonTapped: ()
+@MainActor
+final class SearchModel: ObservableObject {
+    @Published var text: String = ""
+    var searchPublisher: PassthroughSubject<String, Never> = .init()
     
-    init(
-        placeHolder: String,
-        searchText: Binding<String>,
-        clearButtonTapped: Binding<()>
-    ) {
-        self.placeHolder = placeHolder
-        self._searchText = searchText
-        self._clearButtonTapped = clearButtonTapped
+    private var subscriptions: Set<AnyCancellable> = []
+    
+    init() {
+        $text
+            .debounce(for: 0.3, scheduler: RunLoop.main)
+            .sink { [weak self] debouncedText in
+                self?.searchPublisher.send(debouncedText)
+            }
+            .store(in: &subscriptions)
     }
     
-    public var body: some View {
-        ZStack {
-            HStack(alignment: .center, spacing: 12) {
-                if searchText.isEmpty {
-                    Image(systemName: "magnifyingglass")
-                        .frame(width: 16, height: 16)
-                        .foregroundStyle(Color(red: 0.21, green: 0.24, blue: 0.29).opacity(0.6))
-                }
-                
-                Section {
-                    TextField(placeHolder, text: $searchText)
-                    
-                    if searchText.isEmpty {
-                        Button {
-                            clearButtonTapped = ()
-                        } label: {
-                            Image(systemName: "xmark")
-                                .frame(width: 16, height: 16)
-                                .foregroundStyle(Color(red: 0.21, green: 0.24, blue: 0.29).opacity(0.6))
-                        }
-                    }
-                }
-            }
-        }
-        .padding(.horizontal, 16)
-        .padding(.vertical, 7)
-        .background(Color(red: 0.95, green: 0.95, blue: 0.96))
-        .cornerRadius(20)
-        .padding(.bottom, 16)
-        
+    
+    deinit {
+        subscriptions.forEach { $0.cancel() }
     }
 }
 
+public struct KuringTextField: View {
+    @StateObject private var dataModel = SearchModel()
+    
+    let placeholder: String
+    
+    let onSearch: (_ text: String) -> Void
+    let clear: () -> Void
+    
+    public init(
+        placeholder: String,
+        onSearch: @escaping (_ text: String) -> Void,
+        clear: @escaping () -> Void
+    ) {
+        self.placeholder = placeholder
+        self.onSearch = onSearch
+        self.clear = clear
+    }
+    
+    public var body: some View {
+        HStack {
+            if dataModel.text.isEmpty {
+                Image(systemName: "magnifyingglass")
+                    .foregroundStyle(Color(red: 0.21, green: 0.24, blue: 0.29).opacity(0.6))
+            }
+            
+            
+            TextField(placeholder, text: $dataModel.text)
+            
+            if !dataModel.text.isEmpty {
+                Button {
+                    dataModel.text = ""
+                    self.clear()
+                } label: {
+                    Image(systemName: "xmark.circle")
+                        .frame(width: 16, height: 16)
+                        .foregroundStyle(Color(red: 0.21, green: 0.24, blue: 0.29).opacity(0.6))
+                }
+            }
+        }
+        .padding(.horizontal, 20)
+        .frame(height: 40)
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundStyle(Color(red: 0.95, green: 0.95, blue: 0.96))
+        )
+        .padding([.horizontal, .top], 16)
+        .padding(.bottom, 10)
+        .onReceive(dataModel.searchPublisher) { value in
+            self.onSearch(value)
+        }
+    }
+}
+
+
 // TODO: - 테스트 가능한 Preview 고민
 #Preview {
-    @State var searchText: String = ""
-    @State var clearButtonTapped: () = ()
+    @State var searchText: String = "empty"
     
     return VStack {
-        Text("쿠링 SearchField")
-        SearchFiled(placeHolder: "", searchText: $searchText, clearButtonTapped: $clearButtonTapped)
+        KuringTextField(placeholder: "PlaceHolder") { text in
+            searchText = text
+        } clear: {
+            searchText = "clear"
+        }
         
-        Spacer()
+        Text(searchText)
     }
 }
