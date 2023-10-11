f053de804d9d40b90d1a5535eac57b5e1006b3d9
Jaesung Lee <chic0815@konkuk.ac.kr>
2023-10-11 22:18:00 +0900

    [수정] KuringApp 코드 수정 내용 리셋

--- KuringApp/KuringApp/KuringApp.swift
+++ KuringApp/KuringApp/KuringApp.swift
@@ -12,17 +12,7 @@ import ComposableArchitecture
 struct KuringApp: App {
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-            NavigationStack {
-                SearchView(
-                    store: Store(
-                        initialState: SearchFeature.State(recents: ["방학", "운송수단", "운임표"]),
-                        reducer: { SearchFeature() }
-                    )
-                )
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationTitle("Search View")
-            }
+            ContentView()
         }
     }
 }
