a42e7759449f71dd792b8ea9fb1a9ab5ee8a5129
Jaesung Lee <chic0815@konkuk.ac.kr>
2023-12-27 01:00:44 +0900

    [추가] Bookmarks 디펜던시, BookmarkListFeature 도메인, 테스트 타겟 추가 [수정] 테스트 에러 수정 - 올바른 CRUD 로직 [추가] 빌드스크립트에 `BookmarkFeaturesTests`테스트 스크립트 추가 [추가] 테스트 추가

---
+++ KuringPackage/Sources/Caches/Bookmarks.swift
@@ -0,0 +1,100 @@
+import Models
+import Foundation
+import Dependencies
+
+public struct Bookmarks {
+    public var add: (_ notice: Notice) throws -> Void
+    public var remove: (_ id: Notice.ID) throws -> Void
+    public var getAll: () throws  -> [Notice]
+    public init(
+        add: @escaping (_ notice: Notice) throws -> Void,
+        remove: @escaping (_ noidtice: Notice.ID) throws -> Void,
+        getAll: @escaping () throws -> [Notice]
+    ) {
+        self.add = add
+        self.remove = remove
+        self.getAll = getAll
+    }
+    
+    /// 객체를 함수 호출처럼 사용하여 모든 북마크를 가져올 수 있습니다.
+    /// ```swift
+    /// let cachedNotices = try bookmarks()
+    /// ```
+    public func callAsFunction() throws -> [Notice] {
+        try self.getAll()
+    }
+}
+
+extension Bookmarks {
+    public static let `default` = Bookmarks(
+        add: { notice in
+            let folderURL = try FileManager.default.url(
+                for: .documentDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: true
+            )
+            .appendingPathComponent("bookmarks")
+            
+            try FileManager.default.createDirectory(
+                at: folderURL,
+                withIntermediateDirectories: true,
+                attributes: nil
+            )
+            let fileURL = folderURL.appendingPathComponent("\(notice.id).json")
+            
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+            try encoder.encode(notice).write(to: fileURL)
+        },
+        remove: { noticeID in
+            let fileURL = try FileManager.default.url(
+                for: .documentDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: false
+            )
+            .deletingPathExtension()
+            .appendingPathComponent("bookmarks")
+            .appendingPathComponent("\(noticeID).json")
+            
+            if FileManager.default.fileExists(atPath: fileURL.path()) {
+                try FileManager.default.removeItem(atPath: fileURL.path())
+            }
+        },
+        getAll: {
+            let fileURL = try FileManager.default.url(
+                for: .documentDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: true
+            )
+            .appendingPathComponent("bookmarks")
+            
+            let contents = try FileManager.default
+                .contentsOfDirectory(
+                    at: fileURL,
+                    includingPropertiesForKeys: nil
+                )
+            let datas = contents.compactMap { try? Data(contentsOf: $0) }
+            let notices = datas.compactMap {
+                try? JSONDecoder().decode(Notice.self, from: $0)
+            }
+            return notices
+        }
+    )
+    
+}
+
+import ComposableArchitecture
+
+extension Bookmarks: DependencyKey {
+    public static var liveValue: Bookmarks = Bookmarks.default
+}
+
+extension DependencyValues {
+    public var bookmarks: Bookmarks {
+        get { self[Bookmarks.self] }
+        set { self[Bookmarks.self] = newValue }
+    }
+}
---
+++ KuringPackage/Sources/Features/BookmarkFeatures/BookmarkList.swift
@@ -0,0 +1,91 @@
+import Caches
+import Models
+import ComposableArchitecture
+
+/// ```swift
+/// @Bindable var store = Store(initialState: BookmarkListFeature.State())
+/// ```
+@Reducer
+public struct BookmarkListFeature: Reducer {
+    @ObservableState
+    public struct State: Equatable {
+        public var bookmarkedNotices: IdentifiedArrayOf<Notice> = []
+        
+        public var selectedIDs: [Notice.ID] = []
+        
+        public var editMode: EditMode = .none
+        
+        public enum EditMode: Equatable {
+            case `none`
+            case editing(deletable: Bool)
+        }
+        
+        public init(bookmarkedNotices: [Notice] = []) {
+            self.bookmarkedNotices = IdentifiedArray(uniqueElements: bookmarkedNotices)
+        }
+    }
+    
+    public enum Action: Equatable, BindableAction {
+        /// 바인딩
+        case binding(BindingAction<State>)
+        /// 뷰가 나타날 때 가장 첫번째로 수행해야 하는 비동기 액션
+        case onTask
+        /// 북마크 리스트가 업데이트 되면 호출되는 액션
+        case bookmarksUpdate([Notice])
+        /// 편집 버튼 눌렀을 때
+        case editButtonTapped
+        /// 삭제 버튼 눌렀을 때
+        case deleteButtonTapped
+    }
+    
+    /// 북마크 저장소 디펜던시
+    @Dependency(\.bookmarks) public var bookmarks
+    
+    public var body: some ReducerOf<Self> {
+        BindingReducer()
+        
+        Reduce { state, action in
+            switch action {
+            case .binding(\.selectedIDs):
+                switch state.editMode {
+                case .editing:
+                    state.editMode = .editing(deletable: !state.selectedIDs.isEmpty)
+                    return .none
+                default:
+                    return .none
+                }
+                
+            case .onTask:
+                return .run { send in
+                    await send(.bookmarksUpdate(try bookmarks()))
+                }
+                
+            case .bookmarksUpdate(let notices):
+                state.bookmarkedNotices = IdentifiedArray(uniqueElements: notices)
+                return .none
+                
+            case .editButtonTapped:
+                state.editMode = .editing(deletable: false)
+                return .none
+                
+            case .deleteButtonTapped:
+                switch state.editMode {
+                case .editing(deletable: true):
+                    state.selectedIDs.forEach {
+                        state.bookmarkedNotices.remove(id: $0)
+                    }
+                    return .send(.binding(.set(\.selectedIDs, [])))
+                default:
+                    return .none
+                }
+                
+            case .binding:
+                return .none
+            }
+        }
+    }
+    
+    public init() {
+        
+    }
+}
