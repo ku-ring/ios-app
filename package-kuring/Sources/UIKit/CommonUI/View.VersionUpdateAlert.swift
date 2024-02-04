dfd5280016b319a09f780737b9b12f26d28afd87
Jaesung Lee <chic0815@konkuk.ac.kr>
2024-02-04 20:56:56 +0900

    [추가] `VersionUpdateAlert` 테스트 가능하게 생성자 수정 [수정] `versionUpdateAlert()` access level 을 public 으로 수정

--- package-kuring/Sources/UIKit/CommonUI/View.VersionUpdateAlert.swift
+++ package-kuring/Sources/UIKit/CommonUI/View.VersionUpdateAlert.swift
@@ -8,44 +8,45 @@ import Networks
 import Dependencies
 
 /// 최소 지원 버전보다 낮은 경우 업데이트 알림을 띄웁니다.
-/// ```swift
-/// MyView()
-///     .versionUpdateAlert()
-/// ```
 struct VersionUpdateAlert: ViewModifier {
+    /// 테스트를 위한 프로퍼티 입니다.
+    let appVersion: String
+    
     @State private var showsUpdateAlert: Bool = false
     @Dependency(\.appSupport) var appSupport
+    
+    private let appStoreLink = "itms-apps://itunes.apple.com/app/1609873520"
     
     func body(content: Content) -> some View {
         content
             .task {
                 do {
                     showsUpdateAlert = try await appSupport.isAppVersionDeprecated(
-                        KuringLink.appVersion
+                        appVersion
                     )
                 } catch {
                     showsUpdateAlert = false
                 }
             }
-            .alert("업데이트가 필요합니다", isPresented: $showsUpdateAlert) {
+            .alert("업데이트가 필요해요", isPresented: $showsUpdateAlert) {
                 // 무시
                 Button(role: .cancel) {
                     showsUpdateAlert = false
                 } label: {
-                    Text("무시하기")
+                    Text("나중에")
                 }
                 
-                // 업데이트로 연결
+                // 앱스토어 페이지로 연결
                 Button {
                     defer { showsUpdateAlert = false }
-                    guard let url = URL(string: "itms-apps://itunes.apple.com/app/1609873520") else { return }
+                    guard let url = URL(string: appStoreLink) else { return }
                     guard UIApplication.shared.canOpenURL(url) else { return }
                     UIApplication.shared.open(url)
                 } label: {
                     Text("업데이트")
                 }
             } message: {
-                Text("업데이트를 하지 않는 경우 예상치 못한 에러가 발생할 수 있습니다.")
+                Text("업데이트를 진행하여 더욱 안정적이고\n편리한 서비스를 이용해 보세요")
             }
     }
 }
@@ -53,17 +54,18 @@ struct VersionUpdateAlert: ViewModifier {
 extension View {
     /// 현재 앱 버전이 만료되었는지 확인하여 만료된 버전의 경우 업데이트 권장 알림을 띄웁니다.
     /// - Note: 쿠링 앱의 최소 지원버전을 가져와서 비교합니다.
+    /// - Parameter appVersion: 최소 버전과 비교할 앱 버전. 기본값은 `KuringLink.appVersion`.
     /// ```swift
     /// MyView()
     ///     // 현재 버전이 만료된 버전이면 업데이트 권장 알림 띄움
     ///     .versionUpdateAlert()
     /// ```
-    func versionUpdateAlert() -> some View {
-        modifier(VersionUpdateAlert())
+    public func versionUpdateAlert(appVersion: String = KuringLink.appVersion) -> some View {
+        modifier(VersionUpdateAlert(appVersion: appVersion))
     }
 }
 
 #Preview {
     Text("잠시만요")
-        .versionUpdateAlert()
+        .versionUpdateAlert(appVersion: "1.9.0")
 }
