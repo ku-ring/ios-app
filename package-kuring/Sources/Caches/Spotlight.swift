669f9060ad7ec13fdbe1c27942c1344071fa7d34
GeonWoo <lgw9898@naver.com>
2024-02-13 21:59:40 +0900

    [추가] 스포트라이트 기능 구현

---
+++ package-kuring/Sources/Caches/Spotlight.swift
@@ -0,0 +1,37 @@
+//
+//  Spotlight.swift
+//
+//
+//  Created by Geon Woo lee on 2/13/24.
+//
+
+import Models
+
+public struct Spotlight: DependencyKey {
+    public var add: (_ notice: Notice) throws -> Void
+    
+    public init(
+        add: @escaping (_: Notice) -> Void
+    ) {
+        self.add = add
+    }
+}
+
+extension Spotlight {
+    public static let `default` = Spotlight(
+        add: { notice in
+            let attributeSet = CSSearchableItemAttributeSet(contentType: .text)
+        }
+    )
+}
+
+extension Spotlight {
+    public static let liveValue: Spotlight = .default
+}
+
+extension DependencyValues {
+    public var spotlight: Spotlight {
+        get { self[Spotlight.self] }
+        set { self[Spotlight.self] = newValue }
+    }
+}
