d61b95754b16cb687d0e7d43f7d6dab4472e116f
GeonWoo <56182112+lgvv@users.noreply.github.com>
2024-02-18 15:45:47 +0900

    [2.0.0] 스포트라이트 검색 기능 (#139)
    * [추가] 스포트라이트 기능 구현
    
    * [추가] NoticeAppFeature에 스포트라이트 기능 추가 (북마크 시)
    
    * [수정] 라이센스 문구 수정
    
    * [추가] 스포트라이트 add 기능 구현
    
    * [추가] `리뷰` 휴먼에러 방지를 위한 구조 변경
    
    * `peer review` 불필요한 코드를 제거
    
    * [수정] `peer review` domain id 변경
    
    * [수정] throw 제거에 따른 try 제거
    
    * [수정] 테스트 에러 수정 - @Dependency 에 testValue 정의
    
    ---------
    
    Co-authored-by: Jaesung Lee <chic0815@konkuk.ac.kr>

---
+++ package-kuring/Sources/Caches/Spotlight.swift
@@ -0,0 +1,50 @@
+//
+// Copyright (c) 2024 쿠링
+// See the 'License.txt' file for licensing information.
+//
+
+import UIKit
+import Models
+import Dependencies
+import CoreSpotlight
+
+public struct Spotlight: DependencyKey {
+    public var add: (_ notice: Notice) -> Void
+    
+    public init(
+        add: @escaping (_: Notice) -> Void
+    ) {
+        self.add = add
+    }
+}
+
+extension Spotlight {
+    public static let `default` = Spotlight(
+        add: { notice in
+            let attributeSet = CSSearchableItemAttributeSet(contentType: .text)
+            attributeSet.displayName = notice.subject
+            attributeSet.thumbnailData = UIImage(systemName: "AppIcon")?.pngData()
+            
+            let searchableItem = CSSearchableItem(uniqueIdentifier: notice.id,
+                                                  domainIdentifier: "com.kuring.service.bookmarks",
+                                                  attributeSet: attributeSet)
+            
+            CSSearchableIndex.default().indexSearchableItems([searchableItem]) { _ in }
+        }
+    )
+}
+
+extension Spotlight {
+    public static let liveValue: Spotlight = .default
+    
+    public static let testValue: Spotlight = .init(
+        add: { _ in }
+    )
+}
+
+extension DependencyValues {
+    public var spotlight: Spotlight {
+        get { self[Spotlight.self] }
+        set { self[Spotlight.self] = newValue }
+    }
+}
