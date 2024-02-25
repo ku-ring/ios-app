4fd89a7a2892be9dd2268636e5542291dc8069bc
Jaesung Lee <chic0815@konkuk.ac.kr>
2024-02-25 19:40:40 +0100

    [수정] 빈 학과 공지 페이지 디자인 업데이트 반영

--- package-kuring/Sources/UIKit/NoticeUI/NoticeContentView.NoDepartment.swift
+++ package-kuring/Sources/UIKit/NoticeUI/NoticeContentView.NoDepartment.swift
@@ -12,15 +12,13 @@ import ComposableArchitecture
 extension NoticeContentView {
     @ViewBuilder
     func NoDepartmentView() -> some View {
-        VStack(spacing: 0) {
+        VStack(spacing: 32) {
             Spacer()
 
             Text("아직 추가된 학과가 없어요.\n관심 학과를 추가하고 공지를 확인해 보세요!")
                 .font(.system(size: 15, weight: .medium))
                 .multilineTextAlignment(.center)
                 .foregroundColor(.black.opacity(0.36))
-
-            Spacer()
 
             NavigationLink(
                 state: NoticeAppFeature.Path.State.departmentEditor(
@@ -30,6 +28,8 @@ extension NoticeContentView {
                 OverlayButton("학과 추가하기")
                     .padding(.horizontal, 20)
             }
+            
+            Spacer()
         }
     }
 
@@ -37,26 +37,16 @@ extension NoticeContentView {
     /// 상단에 블러가 존재하는 버튼
     @ViewBuilder
     func OverlayButton(_ title: String) -> some View {
-        HStack(alignment: .center, spacing: 10) {
-            Spacer()
+        HStack(alignment: .center, spacing: 8) {
+            Image(systemName: "plus")
 
             Text(title)
                 .font(.system(size: 16, weight: .semibold))
-                .foregroundStyle(Color.white)
-
-            Spacer()
         }
-        .padding(.horizontal, 50)
+        .foregroundStyle(Color.white)
+        .padding(.horizontal, 36)
         .padding(.vertical, 16)
-        .frame(height: 50, alignment: .center)
         .background(Color.accentColor)
         .cornerRadius(100)
-        .background {
-            LinearGradient(
-                gradient: Gradient(colors: [.white.opacity(0.1), .white]),
-                startPoint: .top, endPoint: .bottom
-            )
-            .offset(x: 0, y: -32)
-        }
     }
 }
