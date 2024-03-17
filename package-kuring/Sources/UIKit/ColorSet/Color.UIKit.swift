24f000167c1bcfc0b3aa477b9757638a5d03eb74
GeonWoo <lgw9898@naver.com>
2024-03-18 00:36:52 +0900

    [삭제] 불필요한 코드 삭제

--- package-kuring/Sources/UIKit/ColorSet/Color.UIKit.swift
+++ package-kuring/Sources/UIKit/ColorSet/Color.UIKit.swift
@@ -87,9 +87,9 @@ public struct ColorSet {
     )
     
 }
- 
+
 // MARK: - function
- 
+
 extension ColorSet {
     
     /// SwiftUI Color
@@ -149,15 +149,15 @@ extension ColorSet {
     static func from(hex: String, alpha: CGFloat = 1.0) -> UIColor {
         var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
         hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-                
+        
         var rgb: UInt64 = 0
-                
+        
         Scanner(string: hexSanitized).scanHexInt64(&rgb)
-                
+        
         let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
         let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
         let blue = CGFloat(rgb & 0x0000FF) / 255.0
-                
+        
         return UIColor(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
