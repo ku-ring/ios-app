cbc20f7fd4ff405575b055fa5ed993083293d50f
GeonWoo <lgw9898@naver.com>
2024-03-22 00:44:09 +0900

    [수정] 다크모드 대응

--- package-kuring/Sources/UIKit/DepartmentUI/DepartmentSelector.swift
+++ package-kuring/Sources/UIKit/DepartmentUI/DepartmentSelector.swift
@@ -47,7 +47,7 @@ public struct DepartmentSelector: View {
                     }
                     .padding(.horizontal, 20)
                     .padding(.vertical, 12)
-                    .background(.white)
+                    .background(ColorSet.bg)
                     .onTapGesture {
                         store.send(.selectDepartment(id: department.id))
                     }
--- package-kuring/Sources/UIKit/SettingsUI/OpenSourceList.swift
+++ package-kuring/Sources/UIKit/SettingsUI/OpenSourceList.swift
@@ -4,6 +4,7 @@
 //
 
 import SwiftUI
+import ColorSet
 import SettingsFeatures
 import ComposableArchitecture
 
