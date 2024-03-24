80265e9b485a3ebeed296accfc5a88941d47f1eb
GeonWoo <lgw9898@naver.com>
2024-03-24 22:57:36 +0900

    [추가] Set

--- package-kuring/Sources/Caches/UserDefault.swift
+++ package-kuring/Sources/Caches/UserDefault.swift
@@ -83,3 +83,23 @@ extension Array: RawRepresentable where Element: Codable {
         return result
     }
 }
+
+extension Set: RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode(Set<Element>.self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
+}
