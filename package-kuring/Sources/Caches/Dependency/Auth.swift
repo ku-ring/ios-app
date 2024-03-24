6095a4cce80334f4cb92553e95906cf6ad8cd9bb
GeonWoo <lgw9898@naver.com>
2024-03-25 01:25:41 +0900

    [추가] auth Dependency

--- package-kuring/Sources/Caches/Dependency/Auth.swift
+++ package-kuring/Sources/Caches/Dependency/Auth.swift
@@ -7,6 +7,35 @@ import Models
 import Foundation
 import Dependencies
 
-public Auth {
+/// 쿠링맵 지원하면서 로그인 케이스가 다양해질 것을 고려하여 대응
+public enum AuthTokenType: Codable {
+    case fcm
+}
+
+public struct Auth {
+    /// 토큰 정보
+    public var getToken: (AuthTokenType) -> String?
+    /// 토큰 정보 등록
+    public var registerToken: (_ type: AuthTokenType, _ value: String) -> Void
     
+    @UserDefault(key: StringSet.fcmToken, defaultValue: [:])
+    static var tokenDictionary: [AuthTokenType: String]
+}
+
+extension Auth {
+    public static var `default` = Auth (
+        getToken: { tokenType in
+            switch tokenType {
+            case .fcm:
+                return tokenDictionary[.fcm]
+            }
+            
+        }, registerToken: { type, value in
+            switch type {
+            case .fcm:
+                tokenDictionary[.fcm] = value
+            }
+        }
+    )
+
 }
