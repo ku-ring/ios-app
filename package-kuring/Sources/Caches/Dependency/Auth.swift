c09fdc78bbeef29796c93996d5e8c05e9e1d1363
GeonWoo <56182112+lgvv@users.noreply.github.com>
2024-03-25 23:07:46 +0900

    [2.0.0] Cache > Dependency > Auth (#170)
    * [추가] auth Dependency

---
+++ package-kuring/Sources/Caches/Dependency/Auth.swift
@@ -0,0 +1,52 @@
+//
+// Copyright (c) 2024 쿠링
+// See the 'License.txt' file for licensing information.
+//
+
+import Models
+import Foundation
+import Dependencies
+
+/// 쿠링맵 지원하면서 로그인 케이스가 다양해질 것을 고려하여 대응
+public enum AuthTokenType: Codable {
+    case fcm
+}
+
+public struct Auth {
+    /// 토큰 정보
+    public var getToken: (_ type: AuthTokenType) -> String?
+    /// 토큰 정보 등록
+    public var registerToken: (_ type: AuthTokenType, _ value: String) -> Void
+    
+    @UserDefault(key: StringSet.fcmToken, defaultValue: [:])
+    static var tokenDictionary: [AuthTokenType: String]
+}
+
+extension Auth {
+    public static var `default` = Auth (
+        getToken: { type in
+            switch tokenType {
+            case .fcm:
+                return tokenDictionary[.fcm]
+            }
+            
+        }, registerToken: { type, value in
+            switch type {
+            case .fcm:
+                tokenDictionary[.fcm] = value
+            }
+        }
+    )
+
+}
+
+extension Auth: DependencyKey {
+    public static var liveValue: Auth = .default
+}
+
+extension DependencyValues {
+    public var auth: Auth {
+        get { self[Auth.self] }
+        set { self[Auth.self] = newValue }
+    }
+}
