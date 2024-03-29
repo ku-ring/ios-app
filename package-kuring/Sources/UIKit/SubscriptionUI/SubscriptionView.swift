6d520520d3742c4145142850fa6bca2b9d02f099
Jaesung Lee <chic0815@konkuk.ac.kr>
2024-03-29 16:52:29 +0900

    [수정] `Image(systemName: "circle")` 에 잘못 적용된 색상 올바르게 수정

--- package-kuring/Sources/UIKit/SubscriptionUI/SubscriptionView.swift
+++ package-kuring/Sources/UIKit/SubscriptionUI/SubscriptionView.swift
@@ -125,7 +125,7 @@ struct SubscriptionView: View {
                                         Spacer()
 
                                         Image(systemName: isSelected ? "checkmark.circle.fill" : "circle")
-                                            .foregroundStyle(isSelected ? Color.Kuring.primary : Color.black.opacity(0.1))
+                                            .foregroundStyle(isSelected ? Color.Kuring.primary : Color.Kuring.gray200)
                                             .frame(width: 20, height: 20)
                                     }
                                     .padding(.horizontal, 21.5)
