b3cef9d56264cc7eceb0fe1cd59e5406bad70a6e
GeonWoo <lgw9898@naver.com>
2024-06-05 00:18:41 +0900

    [추가] 위젯 개발을 위한 extension 추가

---
+++ KuringApp/KuringWidgetApp/KuringWidgetAppBundle.swift
@@ -0,0 +1,16 @@
+//
+//  KuringWidgetAppBundle.swift
+//  KuringWidgetApp
+//
+//  Created by Geon Woo lee on 6/5/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct KuringWidgetAppBundle: WidgetBundle {
+    var body: some Widget {
+        KuringWidgetApp()
+    }
+}
