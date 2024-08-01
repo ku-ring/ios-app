50d55d0e6fd15c66b3a3a3324cb25b3e8c76ec47
HyoWon Choi <eve652358@gmail.com>
2024-08-02 00:09:42 +0900

    [2.0.6] 설문조사 팝업 완성

--- KuringApp/KuringApp/ContentView.swift
+++ KuringApp/KuringApp/ContentView.swift
@@ -15,74 +15,81 @@ import SettingsFeatures
 import ComposableArchitecture
 
 struct ContentView: View {
-    @State private var selection: TabBarItem = .notice
-    
-    enum TabBarItem: Hashable {
-        case notice
-        case archive
-        case campusMap
-        case settings
-    }
-    
-    init() {
-        UITabBar.appearance().barTintColor = UIColor.init(Color.Kuring.bg)
-    }
-    
-    var body: some View {
-        TabView(selection: $selection) {
-            NoticeApp(
-                store: Store(
-                    initialState: NoticeAppFeature.State(
-                        noticeList: NoticeListFeature.State()
-                    ),
-                    reducer: { NoticeAppFeature()._printChanges() }
-                )
-            )
-            .tag(TabBarItem.notice)
-            .tabItem {
-                Image(selection == .notice ? .listFill : .list)
-                
-                Text("공지사항")
-            }
-            
-            BookmarkApp(
-                store: Store(
-                    initialState: BookmarkAppFeature.State(
-                        bookmarkList: BookmarkListFeature.State()
-                    ),
-                    reducer: { BookmarkAppFeature() }
-                )
-            )
-            .tag(TabBarItem.archive)
-            .tabItem {
-                Image(selection == .archive ? .archiveFill : .archive)
-                
-                Text("공지보관함")
-            }
-            
-            CampusApp()
-                .tag(TabBarItem.campusMap)
-                .tabItem {
-                    Image(selection == .campusMap ? .mapPinFill : .mapPin)
-                    
-                    Text("캠퍼스맵")
-                }
-            
-            SettingsApp(
-                store: Store(
-                    initialState: SettingsAppFeature.State(),
-                    reducer: { SettingsAppFeature() }
-                )
-            )
-            .tag(TabBarItem.settings)
-            .tabItem {
-                Image(selection == .settings ? .moreHorizontalFill : .moreHorizontal)
-                
-                Text("더보기")
-            }
+  @State private var selection: TabBarItem = .notice
+  
+  enum TabBarItem: Hashable {
+    case notice
+    case archive
+    case campusMap
+    case settings
+  }
+  
+  init() {
+    UITabBar.appearance().barTintColor = UIColor.init(Color.Kuring.bg)
+  }
+  
+  var body: some View {
+    ZStack {
+      TabView(selection: $selection) {
+        NoticeApp(
+          store: Store(
+            initialState: NoticeAppFeature.State(
+              noticeList: NoticeListFeature.State()
+            ),
+            reducer: { NoticeAppFeature()._printChanges() }
+          )
+        )
+        .tag(TabBarItem.notice)
+        .tabItem {
+          Image(selection == .notice ? .listFill : .list)
+          
+          Text("공지사항")
         }
-        .tint(Color.Kuring.gray600)
+        
+        BookmarkApp(
+          store: Store(
+            initialState: BookmarkAppFeature.State(
+              bookmarkList: BookmarkListFeature.State()
+            ),
+            reducer: { BookmarkAppFeature() }
+          )
+        )
+        .tag(TabBarItem.archive)
+        .tabItem {
+          Image(selection == .archive ? .archiveFill : .archive)
+          
+          Text("공지보관함")
+        }
+        
+        CampusApp()
+          .tag(TabBarItem.campusMap)
+          .tabItem {
+            Image(selection == .campusMap ? .mapPinFill : .mapPin)
+            
+            Text("캠퍼스맵")
+          }
+        
+        SettingsApp(
+          store: Store(
+            initialState: SettingsAppFeature.State(),
+            reducer: { SettingsAppFeature() }
+          )
+        )
+        .tag(TabBarItem.settings)
+        .tabItem {
+          Image(selection == .settings ? .moreHorizontalFill : .moreHorizontal)
+          
+          Text("더보기")
+        }
+      }
+      .tint(Color.Kuring.gray600)
+      
+      /// 팝업 추가
+      if PopupView.checkShowPopup() {
+        PopupView()
+      }
     }
+  }
 }
 
 #Preview {
