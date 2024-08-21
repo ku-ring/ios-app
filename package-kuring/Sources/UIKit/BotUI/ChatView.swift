62d7c79b549e82905f8caf87188eef2cea7b5fe8
HyoWon Choi <eve652358@gmail.com>
2024-08-21 18:06:25 +0900

    [수정] ChatView 자동스크롤 구현

--- package-kuring/Sources/UIKit/BotUI/ChatView.swift
+++ package-kuring/Sources/UIKit/BotUI/ChatView.swift
@@ -16,18 +16,26 @@ struct ChatView: View {
     @Query(FetchDescriptor<ChatInfo>()) var chatQuery: [ChatInfo]
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .center, spacing: 16) {
-                ForEach(store.chatHistory) { chat in
-                    chatRow(for: chat)
-                    if chat.type == .answer {
-                        /// 질문 가능 횟수
-                        possibleCountText(for: calculateSendCount(for: chat.index))
+        ScrollViewReader { proxy in
+            ScrollView {
+                LazyVStack(alignment: .center, spacing: 16) {
+                    ForEach(store.chatHistory) { chat in
+                        chatRow(for: chat)
+                        if chat.type == .answer {
+                            possibleCountText(for: calculateSendCount(for: chat.index))
+                                .id(chat.index)
+                        }
                     }
+                    Spacer()
                 }
-                Spacer()
+                .padding(.bottom, 5)
             }
-            .padding(.bottom, 5)
+            .onChange(of: store.chatHistory) { _, value in
+                withAnimation {
+                    proxy.scrollTo(value.last?.index, anchor: .center)
+                    
+                }
+            }
         }
         .onChange(of: self.chatQuery, initial: true) { _, newValue in
             store.send(.queryChanged(newValue))
