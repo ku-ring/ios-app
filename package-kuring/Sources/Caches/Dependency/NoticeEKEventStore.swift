403ce703d682f7f2750c4c4fe3257e8231acb0ef
Jung Hwan Park <hwann1005@gmail.com>
2025-08-16 18:22:09 +0900

    Add NoticeEKEvent dependency

---
+++ package-kuring/Sources/Caches/Dependency/NoticeEKEventStore.swift
@@ -0,0 +1,74 @@
+//
+//  NoticeEKEventStore.swift
+//  package-kuring
+//
+//  Created by Jung Hwan Park on 8/16/25.
+//
+
+import Models
+import EventKit
+import Dependencies
+
+public struct NoticeEKEventStore {
+    /// 전달받은 공지사항으로 EKEvent를 생성
+    public var makeEvent: (_ notice: Notice) -> Void
+    /// 생성된 EKEvent 가져오기
+    public var getEvent: () -> EKEvent?
+    
+    static var event: EKEvent?
+    
+    public init(
+        makeEvent: @escaping (_: Notice) -> Void,
+        getEvent: @escaping () -> EKEvent?
+    ) {
+        self.makeEvent = makeEvent
+        self.getEvent = getEvent
+    }
+}
+
+extension NoticeEKEventStore {
+    public static let `default` = NoticeEKEventStore(
+        makeEvent: { notice in
+            let eventStore = EKEventStore()
+            let event = EKEvent(eventStore: eventStore)
+            let defaults = Self.Default()
+            let structuredLocation = EKStructuredLocation(title: defaults.locationTitle)
+            structuredLocation.geoLocation = defaults.geoLocation
+            
+            event.title = notice.subject
+            event.url = URL(string: notice.url)
+            event.isAllDay = true
+            event.notes = defaults.notes
+            event.alarms = [defaults.alarm]
+            event.structuredLocation = structuredLocation
+            
+            Self.event = event
+        }, getEvent: {
+            return Self.event
+        })
+}
+
+extension NoticeEKEventStore: DependencyKey {
+    public static var liveValue: NoticeEKEventStore = .default
+}
+
+extension DependencyValues {
+    public var noticeEKEventStore: NoticeEKEventStore {
+        get { self[NoticeEKEventStore.self] }
+        set { self[NoticeEKEventStore.self] = newValue }
+    }
+}
+
+extension NoticeEKEventStore {
+    /// 이벤트를 만들때 사용되는 정적 변수
+    struct Default {
+        /// 위치 이름
+        let locationTitle = "건국대학교 05029 대한민국 서울특별시 광진구 능동로 120"
+        /// 위치 좌표
+        let geoLocation = CLLocation(latitude: 37.54360, longitude: 127.07747)
+        /// 24시간 전 알림
+        let alarm = EKAlarm(relativeOffset: -86400)
+        /// 이벤트 상세 내용
+        let notes = "쿠링에서 \(Date.now)에 등록된 일정입니다."
+    }
+}
