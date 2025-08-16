90988d45f02d4ba6dee1e9907f7894540005398c
Jung Hwan Park <hwann1005@gmail.com>
2025-08-16 18:32:41 +0900

    Add EKEventView (공지사항 캘린더 뷰컨)

---
+++ package-kuring/Sources/UIKit/NoticeEKEventUI/EKEventView.swift
@@ -0,0 +1,53 @@
+//
+//  EKEventView.swift
+//  package-kuring
+//
+//  Created by Jung Hwan Park on 8/15/25.
+//
+
+import UIKit
+import Caches
+import SwiftUI
+import EventKit
+import EventKitUI
+import Dependencies
+
+public struct EKEventView: UIViewControllerRepresentable {
+
+    @Environment(\.dismiss) var dismiss
+
+    @Dependency(\.noticeEKEventStore) var noticeEKEventStore
+
+    public init() { }
+
+    public func makeUIViewController(context: Context) -> some UIViewController {
+        let eventEditViewController = EKEventEditViewController()
+        guard let ekEvent = noticeEKEventStore.getEvent() else {
+            return eventEditViewController
+        }
+        
+        eventEditViewController.editViewDelegate = context.coordinator
+        eventEditViewController.eventStore = EKEventStore()
+        eventEditViewController.event = ekEvent
+
+        return eventEditViewController
+    }
+
+    public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+
+    public func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+
+    public class Coordinator: NSObject, EKEventEditViewDelegate {
+        let parent: EKEventView
+
+        init(_ parent: EKEventView) {
+            self.parent = parent
+        }
+
+        public func eventEditViewController(_ controller: EKEventEditViewController, didCompleteWith action: EKEventEditViewAction) {
+            parent.dismiss()
+        }
+    }
+}
