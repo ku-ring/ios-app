044ae45671b80484cdac7106fcaad89a7f04e1e7
Jung Hwan Park <hwann1005@gmail.com>
2025-10-04 18:26:51 +0900

    Update NoticeDetail 리듀서에 댓글 관련 액션 케이스 추가

--- package-kuring/Sources/Features/NoticeFeatures/NoticeDetail.swift
+++ package-kuring/Sources/Features/NoticeFeatures/NoticeDetail.swift
@@ -15,6 +15,7 @@ public struct NoticeDetailFeature {
     @ObservableState
     public struct State: Equatable {
         public var notice: Notice
+        public var comments: CommentData?
         public var isBookmarked: Bool = false
         public var isPresentedEventView: Bool = false
 
@@ -35,6 +36,20 @@ public struct NoticeDetailFeature {
     }
 
     public enum Action: BindableAction, Equatable {
+        case onAppear
+        /// 댓글 GET~
+        case getComments
+        case getCommentsResponse(Result<CommentData, CommentsError>)
+        /// 댓글 추가
+        case addComment(content: String, parentId: Int?)
+        case addCommentResponse(Result<Bool, CommentsError>)
+        /// 댓글 삭제
+        case deleteComment(noticeId: Int, commentId: Int)
+        case deleteCommentResponse(Result<Bool, CommentsError>)
+        /// 댓글 신고
+        case reportComment(commentId: Int, content: String)
+        case reportCommentResponse(Result<Bool, CommentsError>)
+        
         case bookmarkButtonTapped
         case calendarButtonTapped
         case presentEventView
@@ -46,7 +61,20 @@ public struct NoticeDetailFeature {
         public enum Delegate: Equatable {
             case bookmarkUpdated(_ notice: Notice, _ isBookmarked: Bool)
         }
+        
+        public enum CommentsError: Error, Equatable {
+            case error(String)
+            
+            public static func == (lhs: CommentsError, rhs: CommentsError) -> Bool {
+                switch (lhs, rhs) {
+                case let (.error(lmsg), .error(rmsg)):
+                    return lmsg == rmsg
+                }
+            }
+        }
     }
+    
+    @Dependency(\.kuringLink) private var kuringLink
     
     public var body: some ReducerOf<Self> {
         BindingReducer()
@@ -54,6 +82,77 @@ public struct NoticeDetailFeature {
             switch action {
             case .binding:
                 return .none
+            case .onAppear:
+                return .send(.getComments)
+            case .getComments:
+                return .run { [notice = state.notice] send in
+                    do {
+                        let result = try await kuringLink.getComments(notice.id, nil, nil)
+                        await send(.getCommentsResponse(.success(result)))
+                    } catch {
+                        await send(.getCommentsResponse(.failure(.error(error.localizedDescription))))
+                    }
+                }
+            case let .getCommentsResponse(result):
+                switch result {
+                case .success(let comments):
+                    state.comments = comments
+                    return .none
+                case let .failure(error):
+                    print("Get comment response error: \(error)")
+                    return .none
+                }
+            case .addComment(let content, let parentId):
+                return .run { [notice = state.notice] send in
+                    do {
+                        try await kuringLink.addComment(notice.id, content, parentId)
+                        await send(.addCommentResponse(.success(true)))
+                    } catch {
+                        await send(.addCommentResponse(.failure(.error(error.localizedDescription))))
+                    }
+                }
+            case let .addCommentResponse(result):
+                switch result {
+                case .success:
+                    return .send(.getComments)
+                case let .failure(error):
+                    print("Add comment response error: \(error)")
+                    return .none
+                }
+            case .deleteComment(let noticeId, let commentId):
+                return .run { [notice = state.notice] send in
+                    do {
+                        try await kuringLink.deleteComment(noticeId, commentId)
+                        await send(.deleteCommentResponse(.success(true)))
+                    } catch {
+                        await send(.deleteCommentResponse(.failure(.error(error.localizedDescription))))
+                    }
+                }
+            case let .deleteCommentResponse(result):
+                switch result {
+                case .success:
+                    return .send(.getComments)
+                case let .failure(error):
+                    print("Delete comment response error: \(error)")
+                    return .none
+                }
+            case .reportComment(let commentId, let content):
+                return .run { send in
+                    do {
+                        try await kuringLink.reportComment(commentId, content)
+                        await send(.reportCommentResponse(.success(true)))
+                    } catch {
+                        await send(.reportCommentResponse(.failure(.error(error.localizedDescription))))
+                    }
+                }
+            case let .reportCommentResponse(result):
+                switch result {
+                case .success:
+                    return .send(.getComments)
+                case let .failure(error):
+                    print("Report comment response error: \(error)")
+                    return .none
+                }
             case .bookmarkButtonTapped:
                 state.isBookmarked.toggle()
                 return .none
