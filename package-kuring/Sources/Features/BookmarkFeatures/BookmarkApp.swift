6105abcbf4dc9df479fa80027a732c9a6f7d7dc1
Jung Hwan Park <hwann1005@gmail.com>
2025-10-09 22:28:12 +0900

    Update 북마크 패키지에 로그인 기능 추가

--- package-kuring/Sources/Features/BookmarkFeatures/BookmarkApp.swift
+++ package-kuring/Sources/Features/BookmarkFeatures/BookmarkApp.swift
@@ -3,6 +3,7 @@
 // See the 'License.txt' file for licensing information.
 //
 
+import LoginFeatures
 import NoticeFeatures
 import ComposableArchitecture
 
@@ -16,7 +17,7 @@ public struct BookmarkAppFeature {
         public var bookmarkList = BookmarkListFeature.State()
         /// 스택 네비게이션
         public var path = StackState<Path.State>()
-
+        public var signup = EmailVerificationFeature.State()
         public init(
             bookmarkList: BookmarkListFeature.State = BookmarkListFeature.State(),
             path: StackState<Path.State> = StackState<Path.State>()
@@ -29,7 +30,8 @@ public struct BookmarkAppFeature {
     public enum Action {
         /// 루트 액션 (``BookmarkListFeature/Action``)
         case bookmarkList(BookmarkListFeature.Action)
-
+        /// 이메일 인증 네비게이션
+        case signup(EmailVerificationFeature.Action)
         /// 스택 네비게이션 액션 (``BookmarkAppFeature/Path/Action``)
         case path(StackAction<Path.State, Path.Action>)
     }
@@ -39,6 +41,10 @@ public struct BookmarkAppFeature {
     public var body: some ReducerOf<Self> {
         Scope(state: \.bookmarkList, action: \.bookmarkList) {
             BookmarkListFeature()
+        }
+        
+        Scope(state: \.signup, action: \.signup) {
+            EmailVerificationFeature()
         }
 
         Reduce { state, action in
@@ -66,11 +72,69 @@ public struct BookmarkAppFeature {
                         )
                     )
                     return .none
+                case .pushToLogin:
+                    state.path.append(
+                        Path.State.login(
+                            LoginAppFeature.State()
+                        )
+                    )
+                    return .none
                 }
             case let .path(.element(id: _, action: .reportComment(.delegate(.pop)))):
                 state.path.removeLast()
                 return .none
-            case .path, .bookmarkList:
+            case let .path(.element(id: id, action: .setPassword(.delegate(.pushToSignupComplete)))):
+                state.path.append(
+                    Path.State.signupComplete(
+                        SignupCompleteFeature.State()
+                    )
+                )
+                return .none
+            case let .path(.element(id: id, action: .login(.delegate(.popToRoot)))):
+                state.path.removeAll()
+                return .none
+            case let .path(.element(id: id, action: .login(.delegate(.pushToTerms)))):
+                state.path.append(
+                    Path.State.signupTerms(
+                        LoginAppFeature.State()
+                    )
+                )
+                return .none
+            case let .path(.element(id: id, action: .login(.delegate(.pushToFindPassword)))):
+                state.path.append(
+                    Path.State.findPassword(
+                        EmailVerificationFeature.State()
+                    )
+                )
+                return .none
+            case let .path(.element(id: id, action: .signupTerms(.delegate(.pushToSignup)))):
+                state.path.append(
+                    Path.State.signup(
+                        EmailVerificationFeature.State()
+                    )
+                )
+                return .none
+            case let .path(.element(id: id, action: .changePassword(.delegate(.popToRoot)))):
+                state.path.removeAll()
+                return .none
+            case let .path(.element(id: id, action: .signupComplete(.delegate(.popToRoot)))):
+                state.path.removeSubrange(1...)
+                return .none
+            case let .path(.element(id: _, action: .signup(.delegate(.pushToSignupPassword(email))))):
+                state.path.append(
+                    Path.State.setPassword(
+                        SetPasswordFeature.State(email: email)
+                    )
+                )
+                return .none
+            case let .path(.element(id: _, action: .findPassword(.delegate(.pushToChangePassword(email))))):
+                state.path.append(
+                    Path.State.changePassword(
+                        SetPasswordFeature.State(email: email)
+                    )
+                )
+                return .none
+            case .path, .bookmarkList, .signup:
                 return .none
             }
         }
