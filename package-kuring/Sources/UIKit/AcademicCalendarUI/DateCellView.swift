9c9bfd520ba278782f4720300a9d45e41c31d5ab
Jung Hwan Park <hwann1005@gmail.com>
2025-10-22 16:10:10 +0900

    Update DateCellView
    - Use Kuring colors
    - Update padding and frame

--- package-kuring/Sources/UIKit/AcademicCalendarUI/DateCellView.swift
+++ package-kuring/Sources/UIKit/AcademicCalendarUI/DateCellView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ColorSet
 
 struct DateCellView: View {
     let dateInfo: DateInfo
@@ -18,10 +19,11 @@ struct DateCellView: View {
             VStack(spacing: 4) {
                 Text("\(dateInfo.day)")
                     .font(.system(size: 16))
-                    .foregroundColor(isSelected ? .white : (dateInfo.isCurrentMonth ? .black : .gray))
+                    .foregroundColor(isSelected ? Color.Kuring.primary : (dateInfo.isCurrentMonth ? Color.Kuring.title : Color.Kuring.gray300))
                     .background(
                         Circle()
-                            .fill(isSelected ? Color.green : Color.clear)
+                            .fill(isSelected ? Color.Kuring.primarySelected : Color.clear)
+                            .frame(width: 26, height: 26)
                     )
                 
                 if !dots.isEmpty {
@@ -29,13 +31,30 @@ struct DateCellView: View {
                         ForEach(0..<dots.count, id: \.self) { index in
                             Rectangle()
                                 .fill(dots[index])
-                                .frame(width: 8, height: 4)
+                                .frame(width: 5, height: 5)
                         }
                     }
                     .clipShape(Capsule())
+                    .padding(.top, 4)
                 }
             }
         }
         .buttonStyle(PlainButtonStyle())
+    }
+}
+
+#Preview {
+    DateCellView(
+        dateInfo: .init(
+            date: Date(),
+            day: 3,
+            isCurrentMonth: true
+        ),
+        isSelected: true,
+        dots: [
+            .red, .green, .yellow
+        ]
+    ) {
+        print("Tapped")
     }
 }
