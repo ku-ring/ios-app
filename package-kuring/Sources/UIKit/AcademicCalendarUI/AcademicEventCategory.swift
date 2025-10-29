808f2fd6f40ad88bcb5896c012fd683f44fcbefc
Jung Hwan Park <hwann1005@gmail.com>
2025-10-29 23:55:04 +0900

    Update 학사일정 카테고리

--- package-kuring/Sources/UIKit/AcademicCalendarUI/AcademicEventCategory.swift
+++ package-kuring/Sources/UIKit/AcademicCalendarUI/AcademicEventCategory.swift
@@ -10,12 +10,25 @@ import Foundation
 import SwiftUICore
 
 enum AcademicEventCategory: String {
+    /// 기타
     case etc = "ETC"
+    /// 학사운영/행사
+    case academicOperationEvent = "ACADEMIC_OPERATION_EVENT"
+    /// 등록/수강/성적
+    case registrationCourseGrade = "REGISTRATION_COURSE_GRADE"
+    /// 학적/학위
+    case academicDegree = "ACADEMIC_DEGREE"
     
     var color: Color {
         switch self {
         case .etc:
             return Color.Kuring.etc
+        case .academicOperationEvent:
+            return Color.Kuring.event
+        case .registrationCourseGrade:
+            return Color.Kuring.registration
+        case .academicDegree:
+            return Color.Kuring.degree
         }
     }
 }
