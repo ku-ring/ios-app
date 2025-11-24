1c5ae28ac3872e284c4cf97a6cf2a3e0a8a4de2a
Jung Hwan Park <hwann1005@gmail.com>
2025-11-24 21:19:44 +0900

    Add API_HOST plists, Add TargetType

---
+++ Tuist/ProjectDescriptionHelpers/TargetType.swift
@@ -0,0 +1,15 @@
+//
+//  TargetType.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Jung Hwan Park on 11/24/25.
+//
+
+import Foundation
+
+public enum TargetType: String {
+    case core
+    case feature
+    case lab
+    case ui
+}
