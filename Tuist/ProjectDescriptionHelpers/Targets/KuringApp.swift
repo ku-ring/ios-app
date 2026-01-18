5b468b4f1b08da87b2dfbcb39d6fe87662e11658
Jung Hwan Park <hwann1005@gmail.com>
2026-01-18 21:05:31 +0900

    Chore bump version to 2.4.1(1)
    후후 먼지털기

--- Tuist/ProjectDescriptionHelpers/Targets/KuringApp.swift
+++ Tuist/ProjectDescriptionHelpers/Targets/KuringApp.swift
@@ -32,7 +32,7 @@ extension Target {
                 ],
                 "UIDesignRequiresCompatibility": .boolean(true),
                 "CFBundleDisplayName": "쿠링",
-                "CFBundleShortVersionString": "2.3.6",
+                "CFBundleShortVersionString": "2.4.1",
                 "CFBundleVersion": "1",
             ]
         ),
