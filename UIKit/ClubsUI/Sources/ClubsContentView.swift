d6b1c78a3711fd9d0bc52cd0f995ff7296bba457
Jung Hwan Park <hwann1005@gmail.com>
2026-02-09 15:35:00 +0900

    Add sort by view

--- UIKit/ClubsUI/Sources/ClubsContentView.swift
+++ UIKit/ClubsUI/Sources/ClubsContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ColorSet
 
 public struct ClubsContentView: View {
     @State private var selection: String = "전체"
@@ -23,9 +24,40 @@ public struct ClubsContentView: View {
             ClubsTagSelector()
                 .padding(.horizontal, 20)
             
+            sortByView
+                .frame(height: 32)
+                .padding(.top, 16)
+                .padding(.horizontal, 20)
+            
             Spacer()
         }
         .frame(maxWidth: .infinity)
+        .background(Color.Kuring.bg)
+    }
+    
+    private var sortByView: some View {
+        HStack {
+            Text("총 67개")
+                .font(.system(size: 15, weight: .semibold))
+                .foregroundStyle(Color.Kuring.caption1)
+            
+            Spacer()
+            
+            HStack(spacing: 9) {
+                Text("모집 마감일 순")
+                    .font(.system(size: 14, weight: .medium))
+                    .foregroundStyle(Color.Kuring.caption1)
+                
+                Divider()
+                    .frame(width: 1)
+                    .foregroundStyle(Color.Kuring.gray200)
+                    .padding(.vertical, 8)
+                
+                Text("모집 마감일 순")
+                    .font(.system(size: 14, weight: .regular))
+                    .foregroundStyle(Color.Kuring.caption2)
+            }
+        }
     }
 }
 
